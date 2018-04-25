51deef9f345abe94b485e8ee59b55a4705e8010d
dmnsalvo <dmnsalvo@gmail.com>
2018-04-25 14:50:10 -0300

    edit section now visible

--- app/static/js/modal.js
+++ app/static/js/modal.js
@@ -3,16 +3,34 @@ const Modal = (function () {
     /**
      * Abre el modal
      **/
-    function open($modal) {
+    function open($modal, action) {
         const editTitle = document.getElementById('edit-title');
         const saveTitle = document.getElementById('save-title');
         const editButton = document.getElementById('edit-button');
         const saveButton = document.getElementById('save-button');
 
+        const saveSection = document.getElementById('save-section')
+        const editSection = document.getElementById('edit-section')
+
         $modal.classList.add('is-active');
 
-        editButton.classList.add('is-hidden');
-        editTitle.classList.add('is-hidden');
+        if (action === 'add') {
+            editButton.classList.add('is-hidden');
+            editTitle.classList.add('is-hidden');
+            editSection.classList.add('is-hidden')
+
+            saveButton.classList.remove('is-hidden');
+            saveTitle.classList.remove('is-hidden');
+            saveSection.classList.remove('is-hidden')
+        } else if (action === 'edit') {
+            saveButton.classList.add('is-hidden');
+            saveTitle.classList.add('is-hidden');
+            saveSection.classList.add('is-hidden')
+
+            editButton.classList.remove('is-hidden');
+            editTitle.classList.remove('is-hidden');
+            editSection.classList.remove('is-hidden')
+        }  
     }
 
     /**
